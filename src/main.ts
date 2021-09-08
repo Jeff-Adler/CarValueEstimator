17ca8ba35df6c2526de17c37c6bd27e98ec70452
Jeff-Adler <Jeffm.adler@gmail.com>
2021-09-08 14:16:27 -0400

    setup real user create endpoint

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
+      // This will remove any extra properties in the request that is not included in the dto
       whitelist: true,
     }),
   );
