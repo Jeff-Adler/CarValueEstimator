6fcd48629586140643a08e04d01b08f885c3de48
Jeff-Adler <Jeffm.adler@gmail.com>
2021-09-09 16:19:54 -0400

    setup user session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['aergaer'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       // This will remove any extra properties in the request that is not included in the dto
